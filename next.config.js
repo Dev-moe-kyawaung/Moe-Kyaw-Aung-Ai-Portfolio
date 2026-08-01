4524c36bc16211f36e131b03bf691a5144db040b
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-01 12:37:20 +0630

    Create next.config.js

---
+++ next.config.js
@@ -0,0 +1,12 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  reactStrictMode: true,
+  images: {
+    domains: ['gravatar.com', 'images.unsplash.com'],
+  },
+  experimental: {
+    serverActions: true,
+  },
+};
+
+module.exports = nextConfig;
