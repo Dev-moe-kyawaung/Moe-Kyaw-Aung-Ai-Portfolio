4c40cd2b9164ee2fd9bc248c452b21411514df9e
Moe Kyaw Aung <moekyawaung2027@gmail.com>
2026-08-01 12:16:03 +0630

    Create providers.tsx

---
+++ app/providers.tsx
@@ -0,0 +1,31 @@
+'use client';
+
+import { ThemeProvider } from 'next-themes';
+import { useEffect, useState } from 'react';
+
+export function Providers({ children }: { children: React.ReactNode }) {
+  const [mounted, setMounted] = useState(false);
+
+  useEffect(() => {
+    setMounted(true);
+  }, []);
+
+  if (!mounted) {
+    return (
+      <div className="min-h-screen bg-dark-bg">
+        {children}
+      </div>
+    );
+  }
+
+  return (
+    <ThemeProvider
+      attribute="class"
+      defaultTheme="dark"
+      enableSystem
+      disableTransitionOnChange
+    >
+      {children}
+    </ThemeProvider>
+  );
+}
